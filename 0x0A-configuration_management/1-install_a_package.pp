b6acd9a51339b3e01cca6b1ecc9e4fcd30a38124
Trevor <Trevormahinda@gmail.com>
2024-04-19 16:05:53 +0300

    create flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
 # install flask package from pip3
-package {'Flask':
+
+package {'flask':
     ensure   => '2.1.0',
-    provider => 'pip3'
+    provider => 'pip3',
+}
+
+package {'werkzeug':
+    ensure   => '2.1.1',
+    provider => 'pip3',
 }
