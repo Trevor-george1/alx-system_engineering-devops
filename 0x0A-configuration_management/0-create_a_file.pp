a6e896800dd99b31c52421da22c7cc3ae821264d
Trevor <Trevormahinda@gmail.com>
2024-04-19 15:46:29 +0300

    create file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # create a file /tmp/school containing text
 file {'/tmp/school':
-    ensure   => 'present',
-    path     => '/tmp/school',
-    mode     => '0744',
-    owner    => 'www-data',
-    group    => 'www-data',
+    ensure  => 'present',
+    path    => '/tmp/school',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
     content => 'I love Puppet'
 }
