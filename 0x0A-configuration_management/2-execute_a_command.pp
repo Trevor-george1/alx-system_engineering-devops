3a4b28c9f37b782eed9351c720689e410480db1f
Trevor <Trevormahinda@gmail.com>
2024-04-19 21:34:47 +0300

    kill

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 # a manifest that kills a process named killmenow.
 
 exec {'killmenow':
-    command  => 'pkill -f killmenow',
-    path => '/usr/bin',
+    command => 'pkill -f killmenow',
+    path    => '/usr/bin',
 }
