e04ca67fb40e0ea7fc6218a7fb76ad462fda0ddd
Trevor <Trevormahinda@gmail.com>
2024-04-19 15:45:24 +0300

    create file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 # create a file /tmp/school containing text
-
 file {'/tmp/school':
     ensure   => 'present',
+    path     => '/tmp/school',
     mode     => '0744',
     owner    => 'www-data',
     group    => 'www-data',
