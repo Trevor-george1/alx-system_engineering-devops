ca8ad8a4aa94e92686d75f23d46304c22c04bcff
Trevor <Trevormahinda@gmail.com>
2024-04-20 20:29:36 +0300

    clear

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,2 +1,7 @@
 # using puppet to make changes to the default ssh config file
 # so that one can cponnect to a server without tyoing a password.
+
+exec {'ssh_config':
+    path    => '/bin',
+    command => 'echo "PasswordAuthentication no" >> /etc/ssh/ssh_config; echo "IdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config',
+}
