57afa0b43065434842fdfe4a1abdc67cad941393
Trevor <Trevormahinda@gmail.com>
2024-06-04 20:44:40 +0300

    How many subs

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Querying Reddit"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Quering a subreddit and retrieving the no of subscribers"""
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+
+    headers = {'User=Agent': 'My user Agent 1.0'}
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json().get('data', {})
+        sub_count = data.get('subscribers', 0)
+        return sub_count
+    else:
+        return 0
