6ec2dd442d1c961843ec5fe1eb4070465f86e606
root <Trevormahinda>
2024-06-07 16:31:09 +0000

    deebugg

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # fixes Apache 500 error and apache
 
 exec { 'fix-wordpress':
-    command => 'sed -i s/phpp/php/g /var/www/html',
+    command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
     path    => '/usr/local/bin/:/bin/'
 }
